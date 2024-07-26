871a9e8ac3c5b91128f45e98da3d301c5521858e
jinithajohnson <jinithajohnson02@gmail.com>
2024-07-27 00:01:37 +0530

    success

--- app.js
+++ app.js
@@ -2,31 +2,40 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const bcrypt = require("bcryptjs")
-
-const { usermodel } = require("./models/blog")
-
-const app = express()
+const jwt = require("jsonwebtoken")
+const usermodel = require("./models/blog")
+let app = express()
 app.use(cors())
 app.use(express.json())
-
-const generateHashedPassword = async (password) => {
-    const salt = await bcrypt.genSalt(10)
-    return bcrypt.hash(password, salt)
-
-}
-
 mongoose.connect("mongodb+srv://jinithajohnson:jingov02@cluster0.wo3ieyl.mongodb.net/blogDb?retryWrites=true&w=majority&appName=Cluster0")
 
 app.post("/signup", async (req, res) => {
     let input = req.body
-    let hashedPassword = await generateHashedPassword(input.password)
+    let hashedPassword = bcrypt.hashSync(req.body.password,10)
     console.log(hashedPassword)
-    input.password = hashedPassword  //passwordine hashed password akan
-    let blog = new usermodel(input)
-    blog.save()
-    res.json({ "status": "success" })
+    req.body.password=hashedPassword
+
+
+   usermodel.find({ email:req.body.email }).then(
+    (items)=>{
+
+        if (items.length > 0)  {
+            res.json({"status":"email Id already exist"})
+
+        }else  {
+
+            let result=new usermodel(input)
+            result.save()
+            res.json({"status":"success"})
+
+        }
+    }
+   ).catch(
+    (error) => {}
+
+)
 })
 
-app.listen(8080, () => {
+app.listen(8000, () => {
     console.log("server running")
 })
